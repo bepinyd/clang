2fefe83e6f97ab2a8eda40746b8973574a513c70
Bipin Yadav <bepinyd@gmail.com>
2024-06-06 15:51:03 +0530

    chp 19 code here

--- vm 1st/vm.c
+++ vm 1st/vm.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "common.h"
+#include <string.h>
+#include "object.h"
+#include "memory.h"
 #include "debug.h"
 #include "compiler.h"
 #include <stdarg.h>
@@ -21,8 +24,10 @@ resetStack();
 }
 void initVM() {
     resetStack();
+    vm.objects = NULL;
 }
 void freeVM() {
+    freeObjects();
 }
 void push(Value value) {
 *vm.stackTop = value;
@@ -37,6 +42,17 @@ static Value peek(int distance) {
 }
 static bool isFalsey(Value value) {
     return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+static void concatenate() {
+ObjString* b = AS_STRING(pop());
+ObjString* a = AS_STRING(pop());
+int length = a->length + b->length;
+char* chars = ALLOCATE(char, length + 1);
+memcpy(chars, a->chars, a->length);
+memcpy(chars + a->length, b->chars, b->length);
+chars[length] = '\0';
+ObjString* result = takeString(chars, length);
+push(OBJ_VAL(result));
 }
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
@@ -82,8 +98,20 @@ break;
 }
 case OP_GREATER:BINARY_OP(BOOL_VAL, >); break;
 case OP_LESS:BINARY_OP(BOOL_VAL, <); break;
-case OP_ADD:
-BINARY_OP(NUMBER_VAL, +); break;
+case OP_ADD: {
+if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+concatenate();
+} else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+double b = AS_NUMBER(pop());
+double a = AS_NUMBER(pop());
+push(NUMBER_VAL(a + b));
+} else {
+runtimeError(
+"Operands must be two numbers or two strings.");
+return INTERPRET_RUNTIME_ERROR;
+}
+break;
+}
 case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
 case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
 case OP_DIVIDE:
